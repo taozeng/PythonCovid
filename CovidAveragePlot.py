7c45a35f39a4a9ab699d58f0cde6e1310728cd4b
Tao Zeng <tao.zeng@ihsmarkit.com>
2020-08-13 16:53:37 -0700

    Merge states_hash.json and states_population.csv into the script.

--- CovidAveragePlot.py
+++ CovidAveragePlot.py
@@ -7,13 +7,130 @@ from covidtracking import CovidTracking
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 
+state_hash = {
+    "AL": "Alabama",
+    "AK": "Alaska",
+    "AS": "American Samoa",
+    "AZ": "Arizona",
+    "AR": "Arkansas",
+    "CA": "California",
+    "CO": "Colorado",
+    "CT": "Connecticut",
+    "DE": "Delaware",
+    "DC": "District Of Columbia",
+    "FM": "Federated States Of Micronesia",
+    "FL": "Florida",
+    "GA": "Georgia",
+    "GU": "Guam",
+    "HI": "Hawaii",
+    "ID": "Idaho",
+    "IL": "Illinois",
+    "IN": "Indiana",
+    "IA": "Iowa",
+    "KS": "Kansas",
+    "KY": "Kentucky",
+    "LA": "Louisiana",
+    "ME": "Maine",
+    "MH": "Marshall Islands",
+    "MD": "Maryland",
+    "MA": "Massachusetts",
+    "MI": "Michigan",
+    "MN": "Minnesota",
+    "MS": "Mississippi",
+    "MO": "Missouri",
+    "MT": "Montana",
+    "NE": "Nebraska",
+    "NV": "Nevada",
+    "NH": "New Hampshire",
+    "NJ": "New Jersey",
+    "NM": "New Mexico",
+    "NY": "New York",
+    "NC": "North Carolina",
+    "ND": "North Dakota",
+    "MP": "Northern Mariana Islands",
+    "OH": "Ohio",
+    "OK": "Oklahoma",
+    "OR": "Oregon",
+    "PW": "Palau",
+    "PA": "Pennsylvania",
+    "PR": "Puerto Rico",
+    "RI": "Rhode Island",
+    "SC": "South Carolina",
+    "SD": "South Dakota",
+    "TN": "Tennessee",
+    "TX": "Texas",
+    "UT": "Utah",
+    "VT": "Vermont",
+    "VI": "Virgin Islands",
+    "VA": "Virginia",
+    "WA": "Washington",
+    "WV": "West Virginia",
+    "WI": "Wisconsin",
+    "WY": "Wyoming"
+}
 
-def get_state_population(file_name):
+state_population = '''
+Alabama,4903185
+Alaska,731545
+Arizona,7278717
+Arkansas,3017804
+California,39512223
+Colorado,5758736
+Connecticut,3565287
+Delaware,973764
+District of Columbia,705749
+Florida,21477737
+Georgia,10617423
+Hawaii,1415872
+Idaho,1787065
+Illinois,12671821
+Indiana,6732219
+Iowa,3155070
+Kansas,2913314
+Kentucky,4467673
+Louisiana,4648794
+Maine,1344212
+Maryland,6045680
+Massachusetts,6892503
+Michigan,9986857
+Minnesota,5639632
+Mississippi,2976149
+Missouri,6137428
+Montana,1068778
+Nebraska,1934408
+Nevada,3080156
+New Hampshire,1359711
+New Jersey,8882190
+New Mexico,2096829
+New York,19453561
+North Carolina,10488084
+North Dakota,762062
+Ohio,11689100
+Oklahoma,3956971
+Oregon,4217737
+Pennsylvania,12801989
+Rhode Island,1059361
+South Carolina,5148714
+South Dakota,884659
+Tennessee,6829174
+Texas,28995881
+Utah,3205958
+Vermont,623989
+Virginia,8535519
+Washington,7614893
+West Virginia,1792147
+Wisconsin,5822434
+Wyoming,578759
+Puerto Rico,3193694
+'''
+
+
+def get_state_population():
     pop_map = {}
-    with open(file_name) as f:
-        for line in f:
-            state_name, population = line.split(',', )
-            pop_map[str(state_name).lower()] = int(population)
+    for line in state_population.split('\n'):
+        if line:
+            name, population = line.split(',')
+            pop_map[str(name).lower()] = int(population)
     return pop_map
 
 
@@ -121,9 +238,8 @@ def main():
     shown = args.shown
 
     # pre-process
-    state_hash = json.loads(open('states_hash.json').read())
     name_hash = {v.lower(): k for k, v in state_hash.items()}
-    state_pop_map = get_state_population('states_population.csv')
+    state_pop_map = get_state_population()
     plot_chart = True
     if len(states) == 1 and states[0] == 'all':
         states = [name_hash[s] for s in state_pop_map]
